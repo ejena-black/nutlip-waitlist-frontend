d9ed238275dfb45a93040b8787a9af5ef0480aa0
chinwizu <chinwianthony@gmail.com>
2023-05-12 08:04:01 +0100

    hero responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <main className={styles.main}>
     <main className={styles.hero}>
 
-        <div className=' text-[33.67px] lg:mt-20 lg:text-[54px]'>
+        <div className=' mt-20 text-[33.67px] lg:mt-20 lg:text-[54px]'>
           <span className=' leading-[41.75px] font-unbounded block text-center lg:leading-[66.96px] tracking-wide'>Revolutionary <br/> property search portal </span>
         </div>
 
@@ -20,8 +20,8 @@ export default function Home() {
           <span className='text-[16px] lg:text-center leading-[19px] font-urbanist block text-justify lg:text-[18px] lg:leading-[21.6px] tracking-tighter'>Join the waitlist and discover what sets us apart from the rest. Our platform is being <br/> designed to serve Property seekers, Landlords, Estate agents, Mortgage brokers and <br/>Conveyancers. Join us to build that product that serves you better.</span>
         </div>
         <div className='lg:flex flex-col lg:flex-row justify-center lg:gap-[10px] mt-12 font-urbanist'>
-          <input className=' p-[16px] w-[433px] text-black rounded-[8px] bg-[#E7E7E7] border-2 border-[#E7E7E7]' placeholder='Email address' type='text'/>
-          <button className=' mt-4 text-[18px] bg-[#da0025] w-[240px] p-[16px] rounded-[8px]'>Get early access</button>
+          <input className=' p-[16px] w-[95%] lg:w-[433px] text-black rounded-[8px] bg-[#E7E7E7] border-2 border-[#E7E7E7]' placeholder='Email address' type='text'/>
+          <button className=' mt-4 lg:mt-0 text-[18px] bg-[#da0025] w-[240px] p-[16px] rounded-[8px]'>Get early access</button>
         </div>
         <div className='flex justify-center text-[18px] font-urbanist mt-8'>
           <p>Are you an investor? <span className=' underline cursor-pointer'>Click here</span></p>
