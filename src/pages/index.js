e6f432ad98407425eff16d05f5b087fd8279e074
chinwizu <chinwianthony@gmail.com>
2023-05-12 07:53:51 +0100

    still testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,27 +9,19 @@ import lk from '../../public/linkedin.png';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
     <main className={styles.hero}>
-    {/* className='bg-gradient-to-t from-[#3c5aa6] to-[#02133E] xl:h-[100vh] xl:w-[1572px] relative text-white' */}
-       {/* <div className=" w-full h-full object-cover absolute mix-blend-overlay">
-            <Image
-              src={bg}
-              alt='bg house'
-              
-            />
-       </div> */}
-       
-        <div className=' lg:pt-24 text-[54px]'>
-          <span className=' font-unbounded block text-center leading-[66.96px] tracking-wide'>Revolutionary <br/> property search portal </span>
+
+        <div className=' text-[33.67px] lg:mt-20 lg:text-[54px]'>
+          <span className=' leading-[41.75px] font-unbounded block text-center lg:leading-[66.96px] tracking-wide'>Revolutionary <br/> property search portal </span>
         </div>
 
         <div className=' mt-6 flex justify-center'>
-          <span className='text-[16px] leading-[19px] font-urbanist block text-justify lg:text-[18px] lg:leading-[21.6px] tracking-tighter'>Join the waitlist and discover what sets us apart from the rest. Our platform is being <br/> designed to serve Property seekers, Landlords, Estate agents, Mortgage brokers and <br/>Conveyancers. Join us to build that product that serves you better.</span>
+          <span className='text-[16px] lg:text-center leading-[19px] font-urbanist block text-justify lg:text-[18px] lg:leading-[21.6px] tracking-tighter'>Join the waitlist and discover what sets us apart from the rest. Our platform is being <br/> designed to serve Property seekers, Landlords, Estate agents, Mortgage brokers and <br/>Conveyancers. Join us to build that product that serves you better.</span>
         </div>
-        <div className='flex justify-center gap-[10px] mt-12 font-urbanist'>
+        <div className='lg:flex flex-col lg:flex-row justify-center lg:gap-[10px] mt-12 font-urbanist'>
           <input className=' p-[16px] w-[433px] text-black rounded-[8px] bg-[#E7E7E7] border-2 border-[#E7E7E7]' placeholder='Email address' type='text'/>
-          <button className=' text-[18px] bg-[#da0025] w-[240px] p-[16px] rounded-[8px]'>Get early access</button>
+          <button className=' mt-4 text-[18px] bg-[#da0025] w-[240px] p-[16px] rounded-[8px]'>Get early access</button>
         </div>
         <div className='flex justify-center text-[18px] font-urbanist mt-8'>
           <p>Are you an investor? <span className=' underline cursor-pointer'>Click here</span></p>
