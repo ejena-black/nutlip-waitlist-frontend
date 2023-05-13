5249b7540abb833230aef6e16d7febcf784141cb
chinwizu <chinwianthony@gmail.com>
2023-05-13 07:14:04 +0100

    popup web

--- src/pages/invest-form.js
+++ src/pages/invest-form.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import invest from "../../public/invest1.png";
 import arrow from "../../public/arrow.png";
+import { useState } from "react";
 const InvestForm = () => {
+    const [showodal, setShowModal] = useState(false);
+
     return(
         <div className=" lg:flex w-full">
             <div className=" xl:w-[640px]">
@@ -55,12 +58,36 @@ const InvestForm = () => {
                     </div>
 
                     <div className=" px-[8%] lg:px-0 mt-16 flex gap-[8px] w-full">
-                        <div className=" text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
+                        <div onClick={() => setShowModal(true)} className=" text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
                         <div className=" w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Submit</div>
                     </div>
 
                 </div>
+                {
+                    showodal? (
+                        <div className=" fixed inset-0 flex justify-center">
+                            <div className=" px-[87.5px] bg-[#F2F6FF] xl:w-[818px] xl:h-[644px] rounded-[32px] font-urbanist">
+                                <div onClick={()=>setShowModal(false)} className=" cursor-pointer text-[20.4px] font-bold mt-[76.5px] flex justify-end">X</div>
+                        
+                                <div className="flex flex-col gap-[16px]">
+                                    <p className=" font-bold text-[18px] leading-[21.6px]">Invest with nutlip</p>
+                                    <div className=" text-[14px] flex flex-col leading-[16.8px] gap-[16px]"><p>Introducing our exciting new property search portal in order to garner your interest for investment. Our platform seeks to revolutionize the way people search for and invest in real estate properties. We are also leveraging blockchain technology and continue to develop it to ensure quick and transparency of transactions for both users and agents listing on the platform.</p>
+
+                                        <p>We are leveraging technology to create a database of properties that is continually updated with real-time data, ensuring that our users have access to the most relevant and up-to-date property listings.</p>
+
+                                        <p>Our platform will also be providing users with numerous tools to help them make informed decisions about their property purchase, such as detailed property information, location-based insights, and access to experts and resources. We believe that with our platform, users will no longer have to spend endless hours scouring the internet for properties that may or may not meet their needs. Instead, they can trust our platform to help them find and secure their dream property.</p>
+
+                                        <p>We are currently seeking investment to help us bring our platform to the market and scale our operations beyond the shores of the United Kingdom and Europe to the United States of America. We have a talented team with extensive experience in technology and real estate, and we believe that with your investment, we can take our platform to the next level and achieve our growth and expansion goals.</p>
+
+                                        <p>Investing in our platform represents a unique opportunity to be a part of a disruptive and innovative company that is poised to transform the real estate industry. We are confident that our platform will have a positive impact on the lives of many people, and we would be honoured to have you join us on this journey.</p>
+                                    </div>
+                                </div>
                 
+
+                            </div>
+                        </div>
+                    ) : null
+                }
             </div>  
         </div>
     )
--- src/pages/waitlist.js
+++ src/pages/waitlist.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import waitlist from "../../public/waitlist1.png";
 import arrow from "../../public/arrow.png";
+import { useState } from "react";
 const Waitlist = () => {
+    const [showodal, setShowModal] = useState(false);
     return(
         <div className=" lg:flex w-full">
             <div className=" xl:w-[640px]">
@@ -56,11 +58,46 @@ const Waitlist = () => {
                     </div>
 
                     <div className=" mt-16 lg:px-0 flex gap-[8px] px-[8%] w-full">
-                        <div className=" text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
-                        <div className=" w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Join our waitlist</div>
+                        <div onClick={() => setShowModal(true)} className=" cursor-pointer text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
+                        <div className=" cursor-pointer w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Join our waitlist</div>
                     </div>
 
                 </div>
+
+                {
+                    showodal? (
+                        <div className=" fixed inset-0 flex justify-center">
+                            <div className=" px-[45px] bg-[#F2F6FF] xl:w-[1104px] xl:h-[644px] rounded-[32px] font-urbanist">
+                                <div onClick={()=>setShowModal(false)} className=" cursor-pointer text-[20.4px] font-bold mt-[62px] flex justify-end">X</div>
+                                <div className=" flex gap-[32px]">
+                                    <div className=" flex flex-col gap-[24px]">
+                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                            <p className=" text-[18px] leading-[21.6px] font-semibold">Property seekers</p>
+                                            <p className=" text-[14px] leading-[16.8px]">Are you tired of clunky and confusing property search portals with less distinction of property display features as seen in many today? Join our waitlist today and be the first to experience our exciting new platform. Not only will you get an exclusive sneak peek, but you could also be selected as one of the lucky few to help us build the product. Join our community of forward-thinking individuals and revolutionize the way we search for properties. Sign up now for your chance to make an impact with Nutlip.</p>
+                                        </div>
+                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                            <p className=" text-[18px] leading-[21.6px] font-semibold">Mortgage Brokers</p>
+                                            <p className=" text-[14px] leading-[16.8px]">Are you a mortgage broker tired of endless searching for potential clients? Welcome to the ultimate property search portal that not only provides you with leads but help to streamline the entire process from leads to mortgage in principle for clients. With our platform, you won't have to waste your precious time on endless searches for leads as we ensure you receive high-quality leads to help you grow your business. We are excited to announce that we are offering an amazing promotion for the first 1000 mortgage brokers who join our waitlist and subscribe to our platform during and after launch. We are waiving all commission fees for these brokers so they can increase their profits while enjoying our exceptional services. With our cutting-edge technology and user-friendly interface, mortgage brokers can now access a seamless platform that streamlines the lending process, greatly reducing the time and effort required to close deals. By taking advantage of this promotion, brokers will have access to a platform that is designed to make their work easier, faster, and more efficient.</p>
+                                        </div>
+
+                                    </div>
+                                    <div className=" flex flex-col gap-[24px]">
+                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                            <p className=" text-[18px] leading-[21.6px] font-semibold">Estate Agents</p>
+                                            <p className=" text-[14px] leading-[16.8px]">Our platform will be an intuitive, user-friendly, and a secured platform that focuses on providing more efficient and streamlined property search results amongst others increasing your lead conversion rate by 20% using our interactive Picture, Video and Virtual tour viewing windows, never before seen on any UK Property Search Portal. All these and more for less than £50 a month subscription. We are inviting all Estate Agents to join our waitlist, and as an added bonus, we will be giving the first 5000 estate agents that join the waitlist and subscribe to the platform during or after launch, a fantastic offer - 3 free premium listings a month for 1 year. This offer is just one of the ways we plan to reward our members for their trust and help them succeed in all their business endeavors.</p>
+                                        </div>
+                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                            <p className=" text-[18px] leading-[21.6px] font-semibold">Solicitors/Conveyancers</p>
+                                            <p className=" text-[14px] leading-[16.8px]">We designing an efficient and easy-to-use platform that not only helps home shoppers visiting the platform find their dream home or for investment, but also generates high-quality leads for solicitors. As a thank you to the first 500 conveyancers and solicitors who join our waitlist and subscribe to our platform during and after launch, we are proud to offer a special promotion. For an entire year, these early adopters will enjoy a no fee commission on all deals they initiate through our platform. This promotion is designed to make it more affordable for new members to take advantage of our unique features and help them reap the benefits of our platform.</p>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    ) : null
+                }
                 
             </div>  
         </div>
