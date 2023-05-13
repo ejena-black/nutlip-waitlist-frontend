b0bce84b2c47cb2c6e11523484e9677db6665c55
chinwizu <chinwianthony@gmail.com>
2023-05-13 07:54:35 +0100

    checking popup mobile

--- src/pages/invest-form.js
+++ src/pages/invest-form.js
@@ -4,6 +4,7 @@ import arrow from "../../public/arrow.png";
 import { useState } from "react";
 const InvestForm = () => {
     const [showodal, setShowModal] = useState(false);
+    const [showSubmit, setShowSubmit] = useState(false);
 
     return(
         <div className=" lg:flex w-full">
@@ -58,8 +59,8 @@ const InvestForm = () => {
                     </div>
 
                     <div className=" px-[8%] lg:px-0 mt-16 flex gap-[8px] w-full">
-                        <div onClick={() => setShowModal(true)} className=" text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
-                        <div className=" w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Submit</div>
+                        <div onClick={() => setShowModal(true)} className=" cursor-pointer text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
+                        <div onClick={() => setShowSubmit(true)} className=" cursor-pointer w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Submit</div>
                     </div>
 
                 </div>
@@ -71,7 +72,7 @@ const InvestForm = () => {
                         
                                 <div className="flex flex-col gap-[16px]">
                                     <p className=" font-bold text-[18px] leading-[21.6px]">Invest with nutlip</p>
-                                    <div className=" text-[14px] flex flex-col leading-[16.8px] gap-[16px]"><p>Introducing our exciting new property search portal in order to garner your interest for investment. Our platform seeks to revolutionize the way people search for and invest in real estate properties. We are also leveraging blockchain technology and continue to develop it to ensure quick and transparency of transactions for both users and agents listing on the platform.</p>
+                                    <div className=" w-[643px] text-[14px] flex flex-col leading-[16.8px] gap-[16px]"><p>Introducing our exciting new property search portal in order to garner your interest for investment. Our platform seeks to revolutionize the way people search for and invest in real estate properties. We are also leveraging blockchain technology and continue to develop it to ensure quick and transparency of transactions for both users and agents listing on the platform.</p>
 
                                         <p>We are leveraging technology to create a database of properties that is continually updated with real-time data, ensuring that our users have access to the most relevant and up-to-date property listings.</p>
 
@@ -83,6 +84,26 @@ const InvestForm = () => {
                                     </div>
                                 </div>
                 
+
+                            </div>
+                        </div>
+                    ) : null
+                }
+                {
+                    showSubmit? (
+                        <div className=" fixed inset-0 flex justify-center">
+                            <div className=" px-[87.5px] bg-[#F2F6FF] xl:w-[592px] xl:h-[644px] rounded-[32px] font-urbanist">
+                                <div onClick={()=>setShowSubmit(false)} className=" cursor-pointer text-[20.4px] font-bold mt-[76.5px] flex justify-end">X</div>
+                                <div className="flex justify-center items-center">
+                                    <div className=" w-[192px] h-[272px] flex flex-col items-end">
+                                        <div className=" h-[192px] w-[192px]"></div>
+                                        <div className="text-center">
+                                            <p className=" font-bold text-[24px] leading-[28.8px]">Success</p>
+                                            <p className=" text-[16px] leading-[19.2px]">Thank you for signing up</p>
+                                        </div>
+                                      
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
--- src/pages/waitlist.js
+++ src/pages/waitlist.js
@@ -4,6 +4,7 @@ import arrow from "../../public/arrow.png";
 import { useState } from "react";
 const Waitlist = () => {
     const [showodal, setShowModal] = useState(false);
+    const [showSubmit, setShowSubmit] = useState(false);
     return(
         <div className=" lg:flex w-full">
             <div className=" xl:w-[640px]">
@@ -59,7 +60,7 @@ const Waitlist = () => {
 
                     <div className=" mt-16 lg:px-0 flex gap-[8px] px-[8%] w-full">
                         <div onClick={() => setShowModal(true)} className=" cursor-pointer text-[18px] w-[148px] rounded-[8px] border-2 p-[16px] text-center">Learn more</div>
-                        <div className=" cursor-pointer w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Join our waitlist</div>
+                        <div onClick={() => setShowSubmit(true)} className=" cursor-pointer w-[186px] text-[18px] rounded-[8px] lg:w-[292px] bg-[#DA0025] p-[16px] text-center">Join our waitlist</div>
                     </div>
 
                 </div>
@@ -67,26 +68,26 @@ const Waitlist = () => {
                 {
                     showodal? (
                         <div className=" fixed inset-0 flex justify-center">
-                            <div className=" px-[45px] bg-[#F2F6FF] xl:w-[1104px] xl:h-[644px] rounded-[32px] font-urbanist">
+                            <div className=" px-[16px] w-[342px] h-[1470px] xl:px-[45px] bg-[#F2F6FF] xl:w-[1104px] xl:h-[644px] rounded-[32px] font-urbanist">
                                 <div onClick={()=>setShowModal(false)} className=" cursor-pointer text-[20.4px] font-bold mt-[62px] flex justify-end">X</div>
-                                <div className=" flex gap-[32px]">
+                                <div className=" flex flex-col lg:flex lg:gap-[32px]">
                                     <div className=" flex flex-col gap-[24px]">
-                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                        <div className=" w-[310px] flex flex-col gap-[16px] xl:w-[491px]">
                                             <p className=" text-[18px] leading-[21.6px] font-semibold">Property seekers</p>
                                             <p className=" text-[14px] leading-[16.8px]">Are you tired of clunky and confusing property search portals with less distinction of property display features as seen in many today? Join our waitlist today and be the first to experience our exciting new platform. Not only will you get an exclusive sneak peek, but you could also be selected as one of the lucky few to help us build the product. Join our community of forward-thinking individuals and revolutionize the way we search for properties. Sign up now for your chance to make an impact with Nutlip.</p>
                                         </div>
-                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                        <div className=" w-[310px] flex flex-col gap-[16px] xl:w-[491px]">
                                             <p className=" text-[18px] leading-[21.6px] font-semibold">Mortgage Brokers</p>
                                             <p className=" text-[14px] leading-[16.8px]">Are you a mortgage broker tired of endless searching for potential clients? Welcome to the ultimate property search portal that not only provides you with leads but help to streamline the entire process from leads to mortgage in principle for clients. With our platform, you won't have to waste your precious time on endless searches for leads as we ensure you receive high-quality leads to help you grow your business. We are excited to announce that we are offering an amazing promotion for the first 1000 mortgage brokers who join our waitlist and subscribe to our platform during and after launch. We are waiving all commission fees for these brokers so they can increase their profits while enjoying our exceptional services. With our cutting-edge technology and user-friendly interface, mortgage brokers can now access a seamless platform that streamlines the lending process, greatly reducing the time and effort required to close deals. By taking advantage of this promotion, brokers will have access to a platform that is designed to make their work easier, faster, and more efficient.</p>
                                         </div>
 
                                     </div>
                                     <div className=" flex flex-col gap-[24px]">
-                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                        <div className=" w-[310px] flex flex-col gap-[16px] xl:w-[491px]">
                                             <p className=" text-[18px] leading-[21.6px] font-semibold">Estate Agents</p>
                                             <p className=" text-[14px] leading-[16.8px]">Our platform will be an intuitive, user-friendly, and a secured platform that focuses on providing more efficient and streamlined property search results amongst others increasing your lead conversion rate by 20% using our interactive Picture, Video and Virtual tour viewing windows, never before seen on any UK Property Search Portal. All these and more for less than £50 a month subscription. We are inviting all Estate Agents to join our waitlist, and as an added bonus, we will be giving the first 5000 estate agents that join the waitlist and subscribe to the platform during or after launch, a fantastic offer - 3 free premium listings a month for 1 year. This offer is just one of the ways we plan to reward our members for their trust and help them succeed in all their business endeavors.</p>
                                         </div>
-                                        <div className=" flex flex-col gap-[16px] w-[491px]">
+                                        <div className=" w-[310px] flex flex-col gap-[16px] xl:w-[491px]">
                                             <p className=" text-[18px] leading-[21.6px] font-semibold">Solicitors/Conveyancers</p>
                                             <p className=" text-[14px] leading-[16.8px]">We designing an efficient and easy-to-use platform that not only helps home shoppers visiting the platform find their dream home or for investment, but also generates high-quality leads for solicitors. As a thank you to the first 500 conveyancers and solicitors who join our waitlist and subscribe to our platform during and after launch, we are proud to offer a special promotion. For an entire year, these early adopters will enjoy a no fee commission on all deals they initiate through our platform. This promotion is designed to make it more affordable for new members to take advantage of our unique features and help them reap the benefits of our platform.</p>
                                         </div>
@@ -99,6 +100,26 @@ const Waitlist = () => {
                     ) : null
                 }
                 
+                {
+                    showSubmit? (
+                        <div className=" fixed inset-0 flex justify-center">
+                            <div className=" px-[87.5px] bg-[#F2F6FF] xl:w-[592px] xl:h-[644px] rounded-[32px] font-urbanist">
+                                <div onClick={()=>setShowSubmit(false)} className=" cursor-pointer text-[20.4px] font-bold mt-[76.5px] flex justify-end">X</div>
+                                <div className="flex justify-center items-center">
+                                    <div className=" w-[192px] h-[272px] flex flex-col items-end">
+                                        <div className=" h-[192px] w-[192px]"></div>
+                                        <div className="text-center">
+                                            <p className=" font-bold text-[24px] leading-[28.8px]">Success</p>
+                                            <p className=" text-[16px] leading-[19.2px]">Thank you for signing up</p>
+                                        </div>
+                                      
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    ) : null
+                }
             </div>  
         </div>
     )
